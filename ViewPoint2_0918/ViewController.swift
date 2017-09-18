522e8b117caec7069aaba1263189c562aa61078c
D7703_22 <d7703_22@D7703-22ui-iMac.local>
2017-09-18 15:12:37 +0900

    Initial Commit

---
+++ ViewPoint2_0918/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ViewPoint2_0918
+//
+//  Created by D7703_22 on 2017. 9. 18..
+//  Copyright © 2017년 D7703_22. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
